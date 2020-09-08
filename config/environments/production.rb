5158dc9b57b8fd4c708a66e7f325d0dea05ad703
Nemanja Marjanovic <mr.nemanja.marjanovic@gmail.com>
2020-09-08 18:52:51 +0200

    Redis config for Action cable for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.web_socket_server_url = "wss://message-me-chat-app-first.herokuapp.com/cable"
+
+  config.action_cable.allowed_request_origins = ['https://message-me-chat-app-first.herokuapp.com', 'http://message-me-chat-app-first.herokuapp.com']
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
